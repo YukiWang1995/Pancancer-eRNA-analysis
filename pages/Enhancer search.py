7fad6ddd78e722fe37ee600246b9af195af7fae3
YukiWang1995 <157225669+YukiWang1995@users.noreply.github.com>
2024-01-22 17:02:18 +0800

    Enhancer search.py

--- pages/Enhancer search.py
+++ pages/Enhancer search.py
@@ -1,11 +1,15 @@
+import os
 import streamlit as st
 import pandas as pd
 import joblib
 
 import fun.enhancer_gene_corr
 
+aaa=(os.getcwd())
 
 st.header("Cancer Enhancer database")
+st.header(aaa)
+
 
 chr_number = ['chr1', 'chr2', 'chr3','chr4', 'chr5', 'chr6','chr7', 'chr8', 'chr9','chr10', 'chr11', 'chr12','chr13', 'chr14', 'chr15','chr16', 'chr17', 'chr18','chr19', 'chr20', 'chr21','chr22', 'chrX', 'chrY']
 selected_option = st.selectbox('Select an option', chr_number)
